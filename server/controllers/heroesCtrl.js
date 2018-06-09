d0426cb05f194743f5c552d75bb4eff1f3109ae9
Goyo <gregory.vargas18@gmail.com>
2018-06-09 14:47:06 -0500

    POST is functioning properly

--- server/controllers/heroesCtrl.js
+++ server/controllers/heroesCtrl.js
@@ -16,16 +16,26 @@ const getHeroes = (req, res) => {
     res.status(200).send(heroes);
 };
 
-/************** NOT FUNCTIONAL ****************/
+const createHero = (req, res) => {
+  const { name } = req.body;
+  let newHeroId = heroes.length + 1;
+  let newHero = {
+    name,
+    id: newHeroId
+  };
+  heroes.push(newHero);
+  res.status(200).send(heroes);
+};
+
 const deleteHero = (req, res) => {
   const { id } = req.params;
   let heroIndex = heroes.findIndex((hero) => hero.id == id);
   heroes.splice(heroIndex, 1);
   res.status(200).send(heroes);
 };
-/************** NOT FUNCTIONAL ****************/
 
 module.exports = {
   getHeroes,
+  createHero,
   deleteHero
 };
